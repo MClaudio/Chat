0c2168542fc1283dacf4e10cdedb229ae67fa203
Claudio Maldonado <claum0395@gmail.com>
2021-04-19 20:46:46 -0500

    Soket implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const path = require('path');
+const socket = require('socket.io')
 
 const app = express();
-var http = require('http').createServer(app);
+app.set('port', process.env.PORT || 5000);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
-http.listen(5000, () => {
+const server = app.listen(app.get('port'), () => {
     console.log('listening on *:5000');
 });
+
+//config websockets
+const io = socket(server);
+io.on('connection', (soket)=>{
+    console.log("New conection");
+    soket.on('chat:message', (data)=>{
+        //console.log("message", data)
+        soket.broadcast.emit('chat:message', data)
+    });
+
+    soket.on('chat:typing', (data)=>{
+        //console.log("message", data)
+        soket.broadcast.emit('chat:typing', data)
+    });
+
+
+})
---
+++ src/public/chat.js
@@ -0,0 +1,63 @@
+var person = prompt("Cual es tu nombre:");
+while (person == null || person == "") {
+  person = prompt("Cual es tu nombre:");
+}
+
+const socket = io();
+
+const chatContent = document.querySelector(".content-chat");
+const chatText = document.querySelector(".form-control");
+const btnSubmit = document.querySelector(".chat-submit input");
+const chatActions = document.querySelector(".actions");
+const userName = document.getElementById("username");
+
+userName.innerHTML = person;
+
+socket.on("chat:message", (data) => {
+  chatContent.innerHTML += `
+  <div class="d-flex flex-row p-3"> <img
+  src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30"
+  height="30">
+<div class="chat ml-2 p-3">${data.message}</div>
+</div>
+  `;
+});
+
+socket.on("chat:typing", (data) => {
+  if (data.typing) {
+    chatActions.innerHTML = `
+        <div class="d-flex flex-row p-3"> <img
+            src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30"
+            height="30">
+            <div class="chat ml-2 p-3"><span class="text-muted dot">${data.user} esta escribiendo. . .</span></div>
+        </div>
+      `;
+  } else {
+    chatActions.innerHTML = "";
+  }
+});
+
+btnSubmit.addEventListener("click", function () {
+  if (chatText.value !== null && chatText.value !== '') {
+    console.log("chatText.value", chatText.value)
+    socket.emit("chat:message", { user: person, message: chatText.value });
+
+    chatContent.innerHTML += `
+    <div class="d-flex flex-row p-3 my-message">
+        <div class="bg-white mr-2 p-3"><span class="text-muted">${chatText.value}</span></div> <img
+            src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png" width="30"
+            height="30">
+    </div>
+  `;
+
+    chatText.value = "";
+  }
+});
+
+chatText.addEventListener("input", function (evt) {
+  socket.emit("chat:typing", { typing: true, user: person });
+});
+
+chatText.addEventListener("focusout", (evt) => {
+  socket.emit("chat:typing", { typing: false });
+});
